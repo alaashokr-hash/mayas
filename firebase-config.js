590184ee8dc517b50bd69d08624155e31e797438
alaashokr-hash <alaa.shokr@gmail.com>
2026-04-22 21:01:27 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-Jan2M-qMusQQIO8UYcY16MzJfL4ftFg",
+  authDomain: "miyas-school.firebaseapp.com",
+  projectId: "miyas-school",
+  storageBucket: "miyas-school.firebasestorage.app",
+  messagingSenderId: "492549870867",
+  appId: "1:492549870867:web:c5ae4737f9539fc0bc76e6"
+};
+
+export const ADMIN_EMAILS = [
+  "alaashokr@gmail.com"
+];
+
